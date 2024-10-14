8c947b24dd40fbaf5e2f937431c8c7e5ce8a5c3e
Enes <enes.azizi@aartimuhendislik.com>
2024-10-14 09:35:58 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let vid = document.getElementById("myVideo"); 
+
+function playVid() { 
+  vid.play(); 
+} 
+
+function pauseVid() { 
+  vid.pause(); 
+} 
+
+let vid1 = document.getElementById("myVideo1"); 
+
+function playVid1() { 
+  vid1.play(); 
+} 
+
+function pauseVid1() { 
+  vid1.pause(); 
+} 
